dd8b5346041171ca5900dcb0b45817b9648a0df3
Ahmad Hassan <realahmad.ai@gmail.com>
2026-09-19 21:12:56 +0500

    Import Mirror 7 algorithm bundle

---
+++ mirror7_algorithm_01/__init__.py
@@ -0,0 +1,6 @@
+"""
+Mirror 7 Algorithm 01: Open-World Concept Discovery
+"""
+from .mirror7_algorithm import ConceptTree, ConceptNode, Observation
+
+__all__ = ["ConceptTree", "ConceptNode", "Observation"]
