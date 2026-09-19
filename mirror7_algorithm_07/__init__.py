108bc5431e330c289d860bae3d2717cb1ca561e7
Ahmad Hassan <realahmad.ai@gmail.com>
2026-09-19 21:16:08 +0500

    Import Mirror 7 algorithm bundle

---
+++ mirror7_algorithm_07/__init__.py
@@ -0,0 +1,6 @@
+"""
+Mirror 7 Self-Debugging Algorithm (Algorithm 07)
+Meta-cognitive error correction through failure pattern analysis and sandboxed repair.
+"""
+
+from .mirror7_algorithm import SelfDebuggingAlgorithm, FailureRecord, CandidatePatch
