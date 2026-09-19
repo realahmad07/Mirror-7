10ca0a2ae52602a3bfbe015d5956d1b505ed9832
Ahmad Hassan <realahmad.ai@gmail.com>
2026-09-19 18:37:26 +0500

    chore: add Phase 65 package marker

---
+++ phase65_streaming_hidden_state/__init__.py
@@ -0,0 +1,21 @@
+from .mirror7_phase65 import (
+    EffectEstimate,
+    ExperimentPlan,
+    Observation,
+    Phase65Agent,
+    RunningStat,
+    StochasticEffectModel,
+    StreamingHypothesis,
+    StreamingRegimeBank,
+)
+
+__all__ = [
+    "EffectEstimate",
+    "ExperimentPlan",
+    "Observation",
+    "Phase65Agent",
+    "RunningStat",
+    "StochasticEffectModel",
+    "StreamingHypothesis",
+    "StreamingRegimeBank",
+]
