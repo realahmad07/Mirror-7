8488a44f547d7414df0f1630ffa77b3c1b7e73b0
Ahmad Hassan <realahmad.ai@gmail.com>
2026-09-19 21:13:37 +0500

    Import Mirror 7 algorithm bundle

---
+++ mirror7_algorithm_02/__init__.py
@@ -0,0 +1,6 @@
+"""
+Mirror 7 Algorithm 02: Compositional Program Induction
+"""
+from .mirror7_algorithm import ProgramSynthesis, Primitive, Program
+
+__all__ = ["ProgramSynthesis", "Primitive", "Program"]
