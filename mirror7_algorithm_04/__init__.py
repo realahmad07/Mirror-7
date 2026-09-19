f6a5f7206a4c12c97e98e145f078c65e2d5d80a2
Ahmad Hassan <realahmad.ai@gmail.com>
2026-09-19 21:13:58 +0500

    Import Mirror 7 algorithm bundle

---
+++ mirror7_algorithm_04/__init__.py
@@ -0,0 +1,5 @@
+"""
+Adaptive World-Model Builder.
+Constructs a predictive model of an unfamiliar environment and continuously revises it.
+"""
+from .mirror7_algorithm import AdaptiveWorldModelBuilder, TransitionRule
