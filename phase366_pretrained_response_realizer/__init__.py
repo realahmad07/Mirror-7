0ac1490f5dfa142fe2de836e5afd75fb3ed4804b
Ahmad Hassan <realahmad.ai@gmail.com>
2026-09-21 11:42:43 +0500

    Add Phase 366 pretrained response realizer package

---
+++ phase366_pretrained_response_realizer/__init__.py
@@ -0,0 +1,13 @@
+from .mirror7_phase366 import (
+    DEFAULT_MODEL_ID,
+    PretrainedResponseRealizer,
+    build_realizer_prompt,
+    load_pretrained_realizer,
+)
+
+__all__ = [
+    "DEFAULT_MODEL_ID",
+    "PretrainedResponseRealizer",
+    "build_realizer_prompt",
+    "load_pretrained_realizer",
+]
